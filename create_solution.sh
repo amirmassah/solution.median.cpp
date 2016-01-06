fdc9cfd3902d7a7da7de9038ebe6881a396e1b98
Markus Steffes <steffesm@rhrk.uni-kl.de>
2016-01-06 16:07:02 +0100

    adjusted script create_solution.sh

--- create_solution.sh
+++ create_solution.sh
@@ -1,7 +1,8 @@
-source "../settings.sh"
-export REPO_NAME="median.cpp"
+source "settings/settings.sh"
+export REPO_NAME="xor.systemc"
 
 ./clean.sh
 git clone $GITHUB_PREFIX$REPO_NAME".git"
 
 cp files/* -r $REPO_NAME/
+
